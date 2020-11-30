0e3269163883e7c1ec58c201f77f9358658fe68d
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-30 03:11:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  i = 0
   min_value = ""
 hash.each do |name, value|
   if 
@@ -10,4 +11,10 @@ hash.each do |name, value|
 end
 
 min_value
+end
+
+i=0
+for blah in blahs
+    puts i.to_s + " " + blah
+    i+=1
 end
