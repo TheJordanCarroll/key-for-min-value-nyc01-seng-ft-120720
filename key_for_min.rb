410f014028dd513843b2f339999faf9fa9feceaf
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-30 03:34:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,3 +12,14 @@ end
 min_value
 end
 
+def key
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+
